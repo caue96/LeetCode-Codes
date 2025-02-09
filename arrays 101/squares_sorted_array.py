a08b57e4716ea7aa8902201fa5fc0325df21a7da
caue96 <cauemateus@hotmail.com>
2025-02-09 00:55:01 -0300

    Squares of a Sorted Array

---
+++ arrays 101/squares_sorted_array.py
@@ -0,0 +1,36 @@
+'''
+Link to the challenge: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+'''
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        # Ensure nums length is between 1 and 10^4
+        while True:
+            if (1 <= len(nums) <= 10**4):
+                break
+
+        # Ensure i is between -10^4 and 10^4
+        for i in nums:
+            if (-10**4 <= i <= 10**4):
+                break
+
+        n = len(nums)
+        result = [0] * n
+        left, right = 0, n - 1
+        index = n - 1
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result[index] = nums[left] ** 2
+                left = left + 1
+            else:
+                result[index] = nums[right] ** 2
+                right = right - 1
+            index = index - 1
+
+        return result
