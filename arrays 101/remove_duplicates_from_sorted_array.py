4792e8b90c49e4d223ec23b90700fbd52ed532c8
caue96 <cauemateus@hotmail.com>
2025-02-12 11:46:38 -0300

    This code solves another challenge as this one is already the optmized version

--- arrays 101/remove_duplicates_from_sorted_array.py
+++ arrays 101/remove_duplicates_from_sorted_array.py
@@ -1,5 +1,7 @@
 '''
 Link to the challenge: https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
+AND
+Link to the challenge: https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3258/
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
